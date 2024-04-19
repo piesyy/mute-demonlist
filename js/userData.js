46dbe0b092a3aaae0a6e4c24d29c21c6e86ae147
plimbus <161087769+plimbus@users.noreply.github.com>
2024-04-19 03:29:59 -0400

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -46,7 +46,7 @@ function userList() {
                 for (var b = 0 ; b < user_data.length ; b++) {
                     var user_name = user_data[b].name.toUpperCase(); var data_name = entry2.user.toUpperCase();
                     if (user_name == data_name) {
-                        user_data[b].point = user_data[b].point + p * 2;
+                        user_data[b].point = user_data[b].point + p;
                         
                         if (user_data[b].highest == "null" && parseInt(entry2.percent) == 100) {
                             user_data[b].highest = entry.name; 
