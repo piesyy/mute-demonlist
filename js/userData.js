1143dd5d6b8a0860e1f49789808728ab52ad7efe
plimbus <161087769+plimbus@users.noreply.github.com>
2024-04-19 04:05:05 -0400

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -103,7 +103,7 @@ function getUserData(user) {
 
     Swal.fire({
         title : "#"+(user+1)+" : "+user_data[user].name,
-        html : '<center><strong>Score : '+user_data[user].point + '<br>'+
+        html : '<center><strong>Score : '+user_data[user].point * 2 + '<br>'+
             'Best Record : '+user_data[user].highest+'<br>'+
             'Completed Levels : '+clears+' Level(s)<br>'+
             '<br>Record List : '+progresses+'<br>'+
